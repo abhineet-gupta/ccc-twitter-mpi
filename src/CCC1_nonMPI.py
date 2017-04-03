2b6a490e714effccaab893133e9b1b3d55b6d656
Abhineet Gupta <a.gupta1905@gmail.com>
2017-04-03 11:12:42 +1000

    Initial commit

---
+++ src/CCC1_nonMPI.py
@@ -0,0 +1,11 @@
+'''
+Created on 3Apr.,2017
+
+@author: Abhin
+'''
+
+if __name__ == '__main__':
+    print("Hello world!")
+    
+#Regex: \"coordinates\":\[(\d{1,3}\.\d+),(-\d{1,3}\.\d+)]
+# Link: https://regex101.com/r/FMOngC/1
