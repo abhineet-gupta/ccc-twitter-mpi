4eb70be36550171568c9d1a56a483ef764d40c06
Abhineet Gupta <a.gupta1905@gmail.com>
2017-04-03 12:01:18 +1000

    Read coordinates with regex

--- src/CCC1_nonMPI.py
+++ src/CCC1_nonMPI.py
@@ -1,11 +1,33 @@
 '''
-Created on 3Apr.,2017
-
-@author: Abhin
+@author: Abhineet Gupta
 '''
+# Regular Expression library
+import re
+from timeit import default_timer as timer
 
-if __name__ == '__main__':
-    print("Hello world!")
+
+if __name__ == '__main__':    
+    startTime = timer()
     
-#Regex: \"coordinates\":\[(\d{1,3}\.\d+),(-\d{1,3}\.\d+)]
+    testFilePath = 'sampleJSON.txt'
+    tinyTwitterFilePath = 'tinyTwitter.json'
+    
+    # Store coordinates found in file
+    coordinates = []
+    p = re.compile('"coordinates\\":\\[(\\d{1,3}\\.\\d+),(-\\d{1,3}\\.\\d+)]')
+    
+    with open(tinyTwitterFilePath, 'r', encoding='ANSI') as fin:
+        for line in fin:            
+            coordinates.append(p.findall(line))
+        
+    del coordinates[0]
+    del coordinates[-1]
+    print(len(coordinates))
+    print(coordinates[0], coordinates[-1])
+
+    endTime = timer()
+    print(round(endTime-startTime, 2), "secs")
+    
+    
+# Regex: \"coordinates\":\[(\d{1,3}\.\d+),(-\d{1,3}\.\d+)]
 # Link: https://regex101.com/r/FMOngC/1
