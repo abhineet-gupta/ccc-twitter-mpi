31801d3f17943bdd98f083c7bd454dffc6b1c2f7
Abhineet Gupta <a.gupta1905@gmail.com>
2017-04-05 13:40:06 +1000

    MPI fully implemented.

--- src/CCC_A1_MPI.py
+++ src/CCC_A1_MPI.py
@@ -19,6 +19,10 @@ import re   #regex library
 from timeit import default_timer as timer
 import operator
 
+comm.Barrier()
+if proc_rank == 0:
+    start_time = MPI.Wtime()
+
 TwitterFilePath = 'smallTwitter.json'
 gridFilePath = 'melbGrid.json'
 
@@ -34,13 +38,15 @@ grid_boxes = [] # all processes should fill it themselves exactly the same
 tw_coord_narray = None  # each process will get a subset via scatter
 
 # number of coordinates read - each process gets this value broadcasted
-num_coordinates = numpy.zeros(1)
+num_coordinates = numpy.zeros(1, dtype=numpy.int16)
+even_split_idx = numpy.zeros(1, dtype=numpy.int16)
 
 # dict to store each zone and its tweet count
 box_counts = {} # all processes initialise it to zero; but each calculates its own
 
 # tweets that have been boxed
-tweets_categorised = numpy.zeros(1) # each need to calculate its own
+tweets_categorised = numpy.zeros(1, dtype=numpy.int16) # each need to calculate its own
+total_tweets_categorised = numpy.zeros(1, dtype=numpy.int16)
 
 def readJSONTweetFile(filePath, tweets_read):    
     tweets_coordinates = []
@@ -62,7 +68,7 @@ def readJSONGridboxFile(filepath):
             temp_box = grid_patt.findall(line)
             if (temp_box):
                 grid_boxes.append((temp_box[0][0], float(temp_box[0][1]), float(temp_box[0][2]), float(temp_box[0][3]), float(temp_box[0][4])))
-                box_counts[temp_box[0][0]] = numpy.zeros(1)
+                box_counts[temp_box[0][0]] = numpy.zeros(1, dtype=numpy.int16)
 
 def gridifyTweets(tw_coord):
     global box_counts, tweets_categorised
@@ -99,16 +105,14 @@ def tallyTweetsbyRowCol(row_counts, col_counts, box_counts):
 def printStats(sorted_cols, sorted_rows, sorted_zones):
     print("")
     for zone in sorted_zones:
-        print(zone[0] + ":", int(zone[1][0]), "tweets.")
+        print(zone[0] + ":", zone[1][0], "tweets.")
     print("")            
     for row in sorted_rows:
-        print(row[0] + "-Rows:", int(row[1]), "tweets.")
+        print(row[0] + "-Rows:", row[1][0], "tweets.")
     print("")
     for col in sorted_cols:
-        print("Column", col[0] + ":", int(col[1]), "tweets.")
+        print("Column", col[0] + ":", col[1][0], "tweets.")
     print("")
-    
-start_time = timer()
 
 readJSONGridboxFile(gridFilePath)
 final_box_counts = copy.deepcopy(box_counts)
@@ -118,36 +122,31 @@ if proc_rank == 0:
     # Read tweets and grid boxes from JSON files
         # num tweets read (minus the two lines for [ and ])
     tweets_read = [-1]
-    tw_coord_narray = numpy.array(readJSONTweetFile(TwitterFilePath, tweets_read))
+    all_tweet_coords = numpy.array(readJSONTweetFile(TwitterFilePath, tweets_read))
+
     print("# of tweets read:", tweets_read[0])
-    num_coordinates[0] = len(tw_coord_narray)
+    num_coordinates[0] = len(all_tweet_coords)
 
     print("# of Coordinates passed regex:", num_coordinates[0]) 
     print("Num proc:", proc_size)
     print("Num coord:", num_coordinates[0])
+
+    if num_coordinates[0]%proc_size != 0:
+        even_split_idx[0] = num_coordinates[0] - (num_coordinates[0]%proc_size)
+        if proc_rank == 0:
+            gridifyTweets(all_tweet_coords[even_split_idx[0]:])    
+    else:
+        even_split_idx[0] = num_coordinates[0]
     
-    # send num of tweet_coords to each process (Bcast)
-    # split the coords into proc_sized chunks (Scatter?)
-    # send each slice off to the process
-    # each proc gridifies their share
-    # sends their box counts back
-    # root node adds them up
+    tw_coord_narray = all_tweet_coords[:even_split_idx[0]]
 
-    #--------------------------------------------------------------------------------------
-    ##--Parallelise--
-    # Categorize tweets into boxes
-    
-    # gridifyTweets(tw_coord_narray)
-    # print("Total tweets categorised:", tweets_categorised)
-    ##---------------
-
-comm.Bcast(num_coordinates, root=0)
-
-local_tweet_coord = numpy.zeros((int(num_coordinates[0]/proc_size), 2))
+comm.Bcast(even_split_idx, root=0)
+local_tweet_coord = numpy.zeros((int(even_split_idx/proc_size), 2))
 comm.Scatter(tw_coord_narray, local_tweet_coord, root=0)
+#[:even_split_idx]
 
 gridifyTweets(local_tweet_coord)
-print("Total tweets categorised:", tweets_categorised[0])
+print("Tweets processed by process", proc_rank, ":", tweets_categorised[0])
 
 # if proc_rank == 1:
 #     for grid_box in grid_boxes:
@@ -156,19 +155,30 @@ print("Total tweets categorised:", tweets_categorised[0])
 
 for grid_box in grid_boxes:
     comm.Reduce(box_counts[grid_box[0]], final_box_counts[grid_box[0]], op = MPI.SUM)
+    comm.Reduce(tweets_categorised, total_tweets_categorised, op = MPI.SUM)
 
 if proc_rank == 0:
     # count total in rows and columns
     r_counts = {'A':0 , 'B':0, 'C':0, 'D':0}
     c_counts = {'1':0 , '2':0, '3':0, '4':0, '5':0}
+    print("Total tweets categorised:", total_tweets_categorised[0])
     tallyTweetsbyRowCol(r_counts, c_counts, final_box_counts)
         
     # Sort & display zones by counts, rows, columns
-    sorted_zones = sorted(box_counts.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_zones = sorted(final_box_counts.items(), key=operator.itemgetter(1), reverse=True)
     sorted_rows = sorted(r_counts.items(), key=operator.itemgetter(1), reverse=True)
     sorted_cols = sorted(c_counts.items(), key=operator.itemgetter(1), reverse=True)
     printStats(sorted_cols, sorted_rows, sorted_zones)
 
-    # Print processing time
-    end_time = timer()
+# Print processing time
+comm.Barrier()
+if proc_rank == 0:
+    end_time = MPI.Wtime()
     print("Duration:", round(end_time-start_time, 4), "sec(s).")
+
+# send num of tweet_coords to each process (Bcast)
+# split the coords into proc_sized chunks (Scatter?)
+# send each slice off to the process
+# each proc gridifies their share
+# sends their box counts back
+# root node adds them up
